d6a5a1dd651ea2bcf6f5fa5fd868b2488fca4aa1
Kaede Nakayashiki <116351114+naka520@users.noreply.github.com>
2025-12-10 00:14:41 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'github.com',
-        pathname: '/user-attachments/assets/**',
-      },
-    ],
-  },
+    output: 'export',
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'github.com',
+                pathname: '/user-attachments/assets/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
